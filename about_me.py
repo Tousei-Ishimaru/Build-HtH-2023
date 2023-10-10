ea89970cf9e6dce6d24d9fc96e5bab1958ea3862
Tousei-Ishimaru <134813954+Tousei-Ishimaru@users.noreply.github.com>
2023-10-10 16:53:23 -0700

    Initial commit

---
+++ about_me.py
@@ -0,0 +1,7 @@
+fullName = "Tousei Ishimaru"
+nickname = "Tousei"
+age = 17
+haveUsedPython = True
+hobbies = ["volleyball", "music", "small business"]
+favThings = {"movie": "Tokyo drift", "food": "dango", "hobby": "volleyball"}
+all_vars = dict(vars())
